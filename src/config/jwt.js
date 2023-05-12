8415a231891c3b659bc07554cbbd02ebbc2b2c69
joaocarlosrodriguesmaia <joaocarlosrodriguesmaia@gmail.com>
2023-05-11 23:48:31 -0300

    chore: removendo variaveis nao utilizadas

--- src/config/jwt.js
+++ src/config/jwt.js
@@ -1,1 +1,1 @@
-exports.JWT_SECRET = '$2a$08$iWLiM2o6GKPdXXwO!kjaKJK3Vus85oUGkrrcAOc04PzJuBGawO';
+exports.JWT_SECRET = process.env.JWT_SECRET || '$2a$08$iWLiM2o6GKPdXXwO!kjaKJK3Vus85oUGkrrcAOc04PzJuBGawO';
